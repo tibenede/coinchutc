66f661aa7d12534c520db30e1979f9414a33c431
arribbar <bartychou@gmail.com>
2013-10-01 21:06:34 +0200

    ajout de astuces.html et de resultats.html

--- astuces.php
+++
@@ -1,3 +0,0 @@
-<img class="passeMagique" src="./images/passeMagique.jpg" alt="" width="2000px" align="middle" />
-<br><br>
-<img class="baieDesCochons" src="./images/baieDesCochons.jpg" alt="" width="2000px" align="middle" />
